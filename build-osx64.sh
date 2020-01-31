fce579679f45e436cf14ef7ece7dfac002fbe1e0
Thomas Kerin <me@thomaskerin.io>
2020-01-31 20:23:43 +0000

    rm bitcoin dir before platform build

--- build-osx64.sh
+++ build-osx64.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
 host=x86_64-apple-darwin11
-
+rm -rf bitcoin
 git clone https://github.com/cryptocurrency-testing/bitcoin -b $1
 cd bitcoin
 mkdir -p depends/SDKs/
 cp -R ../../../MacOSX10.11.sdk depends/SDKs/
 make -C depends HOST=$host
-echo "finished depends"
-export
-
 ./autogen.sh
 ./configure --with-incompatible-bdb --with-zmq --host=$host --prefix=$PWD/depends/$host
 make HOST=$host
--- build-x86_64-linux-gnu.sh
+++ build-x86_64-linux-gnu.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 host=x86_64-linux-gnu
+rm -rf bitcoin
 git clone https://github.com/cryptocurrency-testing/bitcoin -b $1
 cd bitcoin
-./autogen.sh
 make -j2 -C depends HOST=$host
+./autogen.sh
 ./configure --with-incompatible-bdb --with-zmq --host=$host --prefix=$PWD/depends/$host
 make -j2 HOST=$host
