d23e8e7d8287b2cb34d76befae708320d51fb81c
Thomas Kerin <me@thomaskerin.io>
2020-01-31 04:44:21 +0000

    fix build scripts

--- build-osx64.sh
+++ build-osx64.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+host=x86_64-apple-darwin11
+
 git clone https://github.com/cryptocurrency-testing/bitcoin -b $1
 cd bitcoin
+mkdir -p depends/SDKs/
+cp -R ../../../MacOSX10.11.sdk depends/SDKs/
+make -C depends HOST=$host
+echo "finished depends"
+export
+
 ./autogen.sh
-./configure --with-incompatible-bdb --with-zmq --host=x86_64-apple-darwin11
-make -C depends HOST=x86_64-apple-darwin11
-make HOST=x86_64-apple-darwin11
+./configure --with-incompatible-bdb --with-zmq --host=$host --prefix=$PWD/depends/$host
+make HOST=$host
+make deploy
--- build-x86_64-linux-gnu.sh
+++ build-x86_64-linux-gnu.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+host=x86_64-linux-gnu
 git clone https://github.com/cryptocurrency-testing/bitcoin -b $1
 cd bitcoin
 ./autogen.sh
-./configure --with-incompatible-bdb --with-zmq
-make
+make -j2 -C depends HOST=$host
+./configure --with-incompatible-bdb --with-zmq --host=$host --prefix=$PWD/depends/$host
+make -j2 HOST=$host
