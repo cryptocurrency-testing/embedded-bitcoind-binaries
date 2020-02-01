368fef9b30bcc5cb6388a6445de80cb4c781a1df
Thomas Kerin <me@thomaskerin.io>
2020-02-01 18:40:01 +0000

    fix maybe broken archive name

--- build-osx64.sh
+++ build-osx64.sh
@@ -6,7 +6,7 @@ git clone https://github.com/cryptocurrency-testing/bitcoin -b $1
 cd bitcoin
 mkdir -p depends/SDKs/
 cp -R ../../../MacOSX10.11.sdk depends/SDKs/
-make -j2 -C depends HOST=$host NO_UPNP=1
+make -j2 -C depends HOST=$host NO_UPNP=1 NO_QT=1
 ./autogen.sh
 ./configure --with-incompatible-bdb --disable-bench --disable-tests --with-zmq --host=$host --prefix=$PWD/depends/$host
 make -j2 HOST=$host
