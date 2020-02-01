3edae13d355a45217e7700e309a0c29c75ce03cc
Thomas Kerin <me@thomaskerin.io>
2020-02-01 00:36:41 +0000

    disable tests/bench/gui

--- build-osx64.sh
+++ build-osx64.sh
@@ -8,6 +8,6 @@ mkdir -p depends/SDKs/
 cp -R ../../../MacOSX10.11.sdk depends/SDKs/
 make -j2 -C depends HOST=$host NO_QT=1 NO_UPNP=1
 ./autogen.sh
-./configure --with-incompatible-bdb --with-zmq --host=$host --prefix=$PWD/depends/$host
+./configure --with-incompatible-bdb --with-gui=no --disable-bench --disable-tests --with-zmq --host=$host --prefix=$PWD/depends/$host
 make -j2 HOST=$host
 make deploy
--- build-x86_64-linux-gnu.sh
+++ build-x86_64-linux-gnu.sh
@@ -6,5 +6,5 @@ git clone https://github.com/cryptocurrency-testing/bitcoin -b $1
 cd bitcoin
 make -j2 -C depends HOST=$host NO_QT=1 NO_UPNP=1
 ./autogen.sh
-./configure --with-incompatible-bdb --with-zmq --host=$host --prefix=$PWD/depends/$host
+./configure --with-incompatible-bdb --with-gui=no --disable-bench --disable-tests --with-zmq --host=$host --prefix=$PWD/depends/$host
 make -j2 HOST=$host
