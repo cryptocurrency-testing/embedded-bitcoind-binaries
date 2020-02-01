f825281814644d711812348042881e2fcca61e11
Thomas Kerin <me@thomaskerin.io>
2020-02-01 01:01:38 +0000

    macos: build with gui otherwise make deploy fails

--- build-osx64.sh
+++ build-osx64.sh
@@ -6,8 +6,8 @@ git clone https://github.com/cryptocurrency-testing/bitcoin -b $1
 cd bitcoin
 mkdir -p depends/SDKs/
 cp -R ../../../MacOSX10.11.sdk depends/SDKs/
-make -j2 -C depends HOST=$host NO_QT=1 NO_UPNP=1
+make -j2 -C depends HOST=$host NO_UPNP=1
 ./autogen.sh
-./configure --with-incompatible-bdb --with-gui=no --disable-bench --disable-tests --with-zmq --host=$host --prefix=$PWD/depends/$host
+./configure --with-incompatible-bdb --disable-bench --disable-tests --with-zmq --host=$host --prefix=$PWD/depends/$host
 make -j2 HOST=$host
 make deploy
