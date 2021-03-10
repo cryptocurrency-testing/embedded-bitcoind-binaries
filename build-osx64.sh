6caf54f45371c3d676cd978f235039ed83c734ca
Thomas Kerin <me@thomaskerin.io>
2021-03-10 21:23:32 +0000

    fix error messages

--- build-osx64.sh
+++ build-osx64.sh
@@ -6,12 +6,18 @@ git clone https://github.com/cryptocurrency-testing/bitcoin -b $1
 cd bitcoin
 mkdir -p depends/SDKs/
 cp -R ../../../MacOSX10.11.sdk depends/SDKs/
+ls -lsaht depends/SDKs
+
 make -j2 -C depends HOST=$host NO_UPNP=1 NO_QT=1
 if [ $? != 0 ]; then
-    echo "Make depends - failed"
+    echo "Error: make depends failed"
     exit 1;
 fi
 
 ./autogen.sh
-./configure --with-incompatible-bdb --disable-bench --disable-tests --with-zmq --host=$host --prefix=$PWD/depends/$host
+./configure --with-incompatible-bdb --with-gui=no --disable-bench --disable-tests --with-zmq --host=$host --prefix=$PWD/depends/$host
 make -j2 HOST=$host
+if [ $? != 0 ]; then
+    echo "Error: make failed"
+    exit 1;
+fi
--- build-x86_64-linux-gnu.sh
+++ build-x86_64-linux-gnu.sh
@@ -6,10 +6,14 @@ git clone https://github.com/cryptocurrency-testing/bitcoin -b $1
 cd bitcoin
 make -j2 -C depends HOST=$host NO_UPNP=1 NO_QT=1
 if [ $? != 0 ]; then
-    echo "Make depends - failed"
+    echo "Error: make depends failed"
     exit 1;
 fi
 
 ./autogen.sh
 ./configure --with-incompatible-bdb --with-gui=no --disable-bench --disable-tests --with-zmq --host=$host --prefix=$PWD/depends/$host
 make -j2 HOST=$host
+if [ $? != 0 ]; then
+    echo "Error: make failed"
+    exit 1;
+fi
