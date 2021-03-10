ff5ce784634b2199f6302e6a5025781a1728fdc7
Thomas Kerin <me@thomaskerin.io>
2021-03-10 16:50:44 +0000

    build scripts should error if make depends fails

--- build-osx64.sh
+++ build-osx64.sh
@@ -7,6 +7,11 @@ cd bitcoin
 mkdir -p depends/SDKs/
 cp -R ../../../MacOSX10.11.sdk depends/SDKs/
 make -j2 -C depends HOST=$host NO_UPNP=1 NO_QT=1
+if [ $? != 0 ]; then
+    echo "Make depends - failed"
+    exit 1;
+fi
+
 ./autogen.sh
 ./configure --with-incompatible-bdb --disable-bench --disable-tests --with-zmq --host=$host --prefix=$PWD/depends/$host
 make -j2 HOST=$host
