85d58c45b3805077bc0acf79319b1384b426f007
Thomas Kerin <me@thomaskerin.io>
2021-03-10 21:24:33 +0000

    don't use concurrent make tasks

--- build-osx64.sh
+++ build-osx64.sh
@@ -8,7 +8,7 @@ mkdir -p depends/SDKs/
 cp -R ../../../MacOSX10.11.sdk depends/SDKs/
 ls -lsaht depends/SDKs
 
-make -j2 -C depends HOST=$host NO_UPNP=1 NO_QT=1
+make -C depends HOST=$host NO_UPNP=1 NO_QT=1
 if [ $? != 0 ]; then
     echo "Error: make depends failed"
     exit 1;
@@ -16,7 +16,7 @@ fi
 
 ./autogen.sh
 ./configure --with-incompatible-bdb --with-gui=no --disable-bench --disable-tests --with-zmq --host=$host --prefix=$PWD/depends/$host
-make -j2 HOST=$host
+make HOST=$host
 if [ $? != 0 ]; then
     echo "Error: make failed"
     exit 1;
--- build-x86_64-linux-gnu.sh
+++ build-x86_64-linux-gnu.sh
@@ -4,7 +4,7 @@ host=x86_64-linux-gnu
 rm -rf bitcoin
 git clone https://github.com/cryptocurrency-testing/bitcoin -b $1
 cd bitcoin
-make -j2 -C depends HOST=$host NO_UPNP=1 NO_QT=1
+make -C depends HOST=$host NO_UPNP=1 NO_QT=1
 if [ $? != 0 ]; then
     echo "Error: make depends failed"
     exit 1;
@@ -12,7 +12,7 @@ fi
 
 ./autogen.sh
 ./configure --with-incompatible-bdb --with-gui=no --disable-bench --disable-tests --with-zmq --host=$host --prefix=$PWD/depends/$host
-make -j2 HOST=$host
+make HOST=$host
 if [ $? != 0 ]; then
     echo "Error: make failed"
     exit 1;
